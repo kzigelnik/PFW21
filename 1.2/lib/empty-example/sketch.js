9be191f014777d3bc7d153a8920e67de62d8b7d1
kzigelnik <kzigelnik@mcad.edu>
2021-01-21 17:55:42 -0600

    draw changes

--- 1.2/lib/empty-example/sketch.js
+++ 1.2/lib/empty-example/sketch.js
@@ -1,8 +1,12 @@
 function setup() {
-  // put setup code here
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(204);
-  ellipse(50, 50, 80, 80);
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
