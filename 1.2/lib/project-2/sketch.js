f6a7eafe90f79887862c9aab164e13f4264e3668
kzigelnik <kzigelnik@mcad.edu>
2021-01-21 18:45:05 -0600

    canvas

--- 1.2/lib/project-2/sketch.js
+++ 1.2/lib/project-2/sketch.js
@@ -1,12 +1,8 @@
-function setup() {
-  createCanvas(400, 400);
+function setup() {  
+  createCanvas(480, 120);
 }
 
-function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+function draw() {  
+  background(204);
+  point(240, 60);
 }
