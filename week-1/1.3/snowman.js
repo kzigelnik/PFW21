2f1c1efe6637be055e7f1dc8af9a0adb6714ece6
kzigelnik <kzigelnik@mcad.edu>
2021-01-24 20:26:02 -0600

    arns

--- week-1/1.3/snowman.js
+++ week-1/1.3/snowman.js
@@ -10,12 +10,16 @@ function draw() {
     background(grid);
     //snowman legs
     fill("#f1f1f1");
-    strokeWeight(0);
+    strokeWeight(1);
     stroke(100,250,100);
     // botton body
     ellipse(500, 600, 200, 200);
     // middle body
     ellipse(500, 460, 150, 150);
     // head
-    ellipse(500, 350, 100)
+    ellipse(500, 350, 100);
+    // left arm
+    line(350, 525, 450, 450)
+    // right arm 
+    line(350, 525, 450, 450)
 }
