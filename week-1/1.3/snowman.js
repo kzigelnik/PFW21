3b6b00eb6c65dfa4bf94900e1b546b50bd8154c6
kzigelnik <kzigelnik@mcad.edu>
2021-01-24 18:23:58 -0600

    add grid

--- week-1/1.3/snowman.js
+++ week-1/1.3/snowman.js
@@ -2,4 +2,8 @@ let grid = undefined;
 function setup() {
     createCanvas(1000, 800);
     background ("#ccc");
+    grid = loadImage("100px_grid.png");
+}
+function draw() {
+    background(grid);
 }
