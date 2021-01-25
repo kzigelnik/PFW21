4582bbadc712943701abb63d5500dd6a83075a24
kzigelnik <kzigelnik@mcad.edu>
2021-01-24 19:31:08 -0600

    update

--- week-1/1.3/snowman.js
+++ week-1/1.3/snowman.js
@@ -1,20 +1,16 @@
-const { FILL, STROKE } = require("../p5");
-
 let grid = undefined;
-function preload() {
-    img = loadImage("images/100px_grid.png");
-}
 
 function setup() {
     createCanvas(1000, 800);
     background ("#ccc");
+    grid = loadImage("images/100px_grid.png");
 }
+
 function draw() {
     background(grid);
-    Image(img, 0, 0);
     //snowman legs
     fill("#f1f1f1");
-    STROKE(100,250,100);
+    stroke(100,250,100);
     // left
     ellipse(350,650, 200);
 
