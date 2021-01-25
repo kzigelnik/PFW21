a7f8aa2ff48d1beb1c5a46ce12169c58c0cd2340
kzigelnik <kzigelnik@mcad.edu>
2021-01-24 18:50:46 -0600

    image

--- week-1/1.3/snowman.js
+++ week-1/1.3/snowman.js
@@ -1,9 +1,13 @@
 let grid = undefined;
+function preload() {
+    img = loadImage("images/100px_grid.png");
+}
+
 function setup() {
     createCanvas(1000, 800);
     background ("#ccc");
-    grid = loadImage("images/100px_grid.png");
 }
 function draw() {
     background(grid);
+    Image(img, 0, 0)
 }
