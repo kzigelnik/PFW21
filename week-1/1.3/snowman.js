34bf67766ee10e12d48d39d480358b7dabc8b382
kzigelnik <kzigelnik@mcad.edu>
2021-01-24 18:32:48 -0600

    commit

--- week-1/1.3/snowman.js
+++ week-1/1.3/snowman.js
@@ -2,7 +2,7 @@ let grid = undefined;
 function setup() {
     createCanvas(1000, 800);
     background ("#ccc");
-    grid = loadImage("100px_grid.png");
+    grid = loadImage("images/100px_grid.png");
 }
 function draw() {
     background(grid);
