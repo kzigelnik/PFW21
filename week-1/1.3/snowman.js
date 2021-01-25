c08a85f71d78c7a810a1af9b5371dcd49884c252
kzigelnik <kzigelnik@mcad.edu>
2021-01-24 19:24:56 -0600

    snowman legs

--- week-1/1.3/snowman.js
+++ week-1/1.3/snowman.js
@@ -1,3 +1,5 @@
+const { FILL, STROKE } = require("../p5");
+
 let grid = undefined;
 function preload() {
     img = loadImage("images/100px_grid.png");
@@ -9,5 +11,11 @@ function setup() {
 }
 function draw() {
     background(grid);
-    Image(img, 0, 0)
+    Image(img, 0, 0);
+    //snowman legs
+    fill("#f1f1f1");
+    STROKE(100,250,100);
+    // left
+    ellipse(350,650, 200);
+
 }
