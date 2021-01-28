32024461ebd9f0da313df2761b444d8057e1502b
kzigelnik <kzigelnik@mcad.edu>
2021-01-28 13:52:48 -0600

    array final

---
+++ week-2/2.1/arrays.js
@@ -0,0 +1,7 @@
+const favoriteMovie = prompt("what is your favorite movie?"); 
+
+let myArr = ['The Graduate', 'Children of Men', 'Howls Moving Castle', 'Call Me By Your Name', 'Her'];
+
+myArr.push(favoriteMovie);
+console.log(myArr);
+console.log(myArr .toString());s
