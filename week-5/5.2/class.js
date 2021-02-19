39b2e84c979b26484b64c9a5dcb5a7900aa46b28
kzigelnik <kzigelnik@mcad.edu>
2021-02-18 23:07:18 -0600

    update

--- week-5/5.2/class.js
+++ week-5/5.2/class.js
@@ -1,39 +1,39 @@
-let bubble1;
-let bubble2;
-let bubble3;
+let squareArray = [];
 
 function setup () {
   createCanvas(600, 400);
-  bubble1 = new Bubble();
-  bubble2 = new Bubble();
-  bubble3 = new Bubble();
+  for (let i = 0; i < 6; i++) {
+    const square = new Square(200, 200, 25, 25);
+   squareArray.push(square);
+  }
 }
-
-function draw() {
+function draw () {
   background('darkBlue');
-  bubble1.move();
-  bubble1.show();
-  bubble2.move();
-  bubble2.show();
-  bubble3.move();
-  bubble3.show();
+  for (let k = 0; k < 3; k++) {
+    squareArray[k].move();
+    squareArray[k].show();
+  }
 }
-
-class Bubble {
-  constructor() {
-    this.x = 200;
-    this.y = 150;
+// classes are new blocks of code that contain everything it means to be that object: functionality, data, etc. The class is the cookie cutter.
+class Square {
+  constructor (x, y, w, h) {
+    // 'this' keyword references the current object
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
   }
 
-  move() {
-    this.x = this.x + random(-5, 5);
-    this.y = this.y + random(-5, 5);
+  move () {
+    // the numbers in random are the speed at which the shape moves
+    this.x = this.x + random(-2, 2);
+    this.y = this.y + random(-2, 2);
   }
 
-  show() {
-    stroke(255);
-    strokeWeight(4);
-    noFill(4);
-    square(this.x, this.y, 24);
+  show () {
+    stroke('yellow');
+    strokeWeight(7);
+    noFill();
+    rect(this.x, this.y, this.w, this.h);
   }
 }
