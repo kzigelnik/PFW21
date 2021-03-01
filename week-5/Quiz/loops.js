bdda17803be72d185663b19d9dc6dca43f58b698
kzigelnik <kzigelnik@mcad.edu>
2021-02-28 21:07:21 -0600

    update 6.a

--- week-5/Quiz/loops.js
+++
@@ -1,8 +0,0 @@
-
-var nameOptions = [ 'Lucky', 'Lucy', 'Limda','Lacy'. 'Carl' ];
-var text = "";
-var i;
-for (i = 0; i < nameOptions.length; i++) {
-  text += "My dog " + nameOptions[i] + " is a really good puppy."
-}
-
