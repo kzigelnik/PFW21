bad8856e9afaed3b9d8751d8ec9333a3e04420ff
kzigelnik <kzigelnik@mcad.edu>
2021-02-28 19:57:51 -0600

    update 6.a

---
+++ week-6 copy/6.a/llamas.js
@@ -0,0 +1,20 @@
+// Add your code here
+// new instance of the Vue class 
+new Vue({
+  el: "#myApp",
+  data: {
+    message: "Hello World!",
+}
+});
+
+  new Vue({
+    el: "#subHead", 
+    data: {
+      message: "Here is the first paragraph of the Grame of Thrones script."}
+  });
+
+new Vue({
+    el: "#quote", 
+    data: {
+      message: "Snow drifts accros the bodies of the fallen dead. Eight corpses lie frozen on the grounf -- men, women, and children, wearing heavy furs. The wind whips through their long hair."}
+  });
---
+++ week-6/6.a/llamas.js
@@ -0,0 +1,55 @@
+// Add your code here
+
+new Vue({
+    el: "#llamaFactOne'",
+    data: { 
+      message: "Llamas are members of the camelid family meaning they are pretty closely related to vicuñas and camels."}
+  });
+  
+  new Vue({
+    el: "#llamaFactTwo",
+    data: { 
+      message: "Camelids first appeared on the Central Plains of North America about 40 million years ago. About 3 million years ago, llamas ancestors migrated to South America.Here is the first paragraph of the Grame of Thrones script."}
+  });
+  
+  new Vue({
+    el: "#llamaFactThree",
+    data: { 
+      message: "During the last ice age (10,000-12,000 years ago) camelids went extinct in North America. Now there are around 160,000 llamas and 100,000 alpacas in the United States and Canada."}
+  });
+  
+  new Vue({
+    el: "#llamaFactFour",
+    data: { 
+      message: "Llamas were first domesticated and used as pack animals 4,000 to 5,000 years ago in the Peruvian highlands."}
+  });
+  
+  new Vue({
+    el: "#llamaFactSix", 
+    data: { 
+      message: "Llamas can grow as much as 6 feet tall though the average llama between 5 feet 6 inches and 5 feet 9 inches tall."}
+  });
+  
+  new Vue({
+    el: "#llamaFactSeven",
+    data: { 
+      message: "Llamas weigh between 280 and 450 pounds and can carry 25 to 30 percent of their body weight, so a 400-pound male llama can carry about 100 to 120 pounds on a trek of 10 to 12 miles with no problem."}
+  });
+  
+  new Vue({
+    el: "#llamaFactEight",
+    data: { 
+      message: "Llamas know their own limits. If you try to overload a llama with too much weight, the llama is likely to lie down or simply refuse to move."}
+  });
+  
+  new Vue({
+    el: "#llamaFactNine",
+    data: { 
+      message: "In the Andes Mountains of Peru, llama fleece has been shorn and used in textiles for about 6,000 years. Llama wool is light, warm, water-repellent, and free of lanolin."}
+    });
+  
+  new Vue({
+    el: "#llamaFactTen",
+    data: { 
+      message: "Llamas are smart and easy to train."}
+  });
