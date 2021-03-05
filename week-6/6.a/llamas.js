ac7d223a14fa647357e845bdbdcd79a46bb416d0
kzigelnik <kzigelnik@mcad.edu>
2021-03-04 18:40:10 -0600

    update 7.1

--- week-6/6.a/llamas.js
+++ week-6/6.a/llamas.js
@@ -1,72 +1,17 @@
 // Add your code here
 
 new Vue({
-  el: '#llamaOne',
+  el: '#llamaFacts',
   data: {
-    message: 'Llamas are members of the camelid family meaning they are pretty closely related to vicunas and camels.' 
+    llamaOne: 'Llamas are members of the camelid family meaning they are pretty closely related to vicunas and camels.',
+    llamaTwo: 'Camelids first appeared on the Central Plains of North America about 40 million years ago. About 3 million years ago, llamas ancestors migrated to South America.Here is the first paragraph of the Grame of Thrones script.',
+    llamaThree: 'During the last ice age (10,000-12,000 years ago) camelids went extinct in North America. Now there are around 160,000 llamas and 100,000 alpacas in the United States and Canada.',
+    llamaFour: 'Llamas were first domesticated and used as pack animals 4,000 to 5,000 years ago in the Peruvian highlands.',
+    llamaFive: 'The current population of llamas and alpacas in South America is estimated to be more than 7 million.',
+    llamaSix: 'Llamas can grow as much as 6 feet tall though the average llama between 5 feet 6 inches and 5 feet 9 inches tall.',
+    llamaSeven: 'Llamas weigh between 280 and 450 pounds and can carry 25 to 30 percent of their body weight, so a 400-pound male llama can carry about 100 to 120 pounds on a trek of 10 to 12 miles with no problem.',
+    llamaEight: 'Llamas know their own limits. If you try to overload a llama with too much weight, the llama is likely to lie down or simply refuse to move.',
+    llamaNine: 'In the Andes Mountains of Peru, llama fleece has been shorn and used in textiles for about 6,000 years. Llama wool is light, warm, water-repellent, and free of lanolin.',
+    llamaTen: 'Llamas are smart and easy to train.'
   }
-});
-
-new Vue({
-  el: '#llamaTwo',
-  data: {
-    message: 'Camelids first appeared on the Central Plains of North America about 40 million years ago. About 3 million years ago, llamas ancestors migrated to South America.Here is the first paragraph of the Grame of Thrones script.' 
-  }
-});
-
-new Vue({
-  el: '#llamaThree',
-  data: {
-    message: 'During the last ice age (10,000-12,000 years ago) camelids went extinct in North America. Now there are around 160,000 llamas and 100,000 alpacas in the United States and Canada.'
-  }
-});
-
-new Vue({
-  el: '#llamaFour',
-  data: {
-    message: 'Llamas were first domesticated and used as pack animals 4,000 to 5,000 years ago in the Peruvian highlands.'
-  }
-});
-
-new Vue({
-  el: '#llamaFive', 
-  data: { 
-    message: 'The current population of llamas and alpacas in South America is estimated to be more than 7 million.'
-  }
-});
-
-new Vue({
-  el: '#llamaSix', 
-  data: { 
-    message: 'Llamas can grow as much as 6 feet tall though the average llama between 5 feet 6 inches and 5 feet 9 inches tall.'
-  }
-});
-
-new Vue({
-  el: '#llamaSeven',
-  data: {
-    message: 'Llamas weigh between 280 and 450 pounds and can carry 25 to 30 percent of their body weight, so a 400-pound male llama can carry about 100 to 120 pounds on a trek of 10 to 12 miles with no problem.'
-  }
-});
-
-new Vue({
-  el: '#llamaEight',
-  data: {
-    message: 'Llamas know their own limits. If you try to overload a llama with too much weight, the llama is likely to lie down or simply refuse to move.'
-  }
-});
-
-new Vue({
-  el: '#llamaNine',
-  data: {
-  message: 'In the Andes Mountains of Peru, llama fleece has been shorn and used in textiles for about 6,000 years. Llama wool is light, warm, water-repellent, and free of lanolin.' 
-}
-  });
-
-new Vue({
-  el: '#llamaTen',
-  data: {
-  message: 'Llamas are smart and easy to train.',
-  hiddenFact: true
-  }
-});
+})
