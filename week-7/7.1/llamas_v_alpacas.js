c94341f3297f7793dda8ae2c63e8aa3bfb22d47b
kzigelnik <kzigelnik@mcad.edu>
2021-03-05 10:26:50 -0600

    Update with Style

--- week-7/7.1/llamas_v_alpacas.js
+++ week-7/7.1/llamas_v_alpacas.js
@@ -1,14 +1,15 @@
 // Add your code here
 
-new Vue({
+var vm = new Vue({
   el: '#lva',
   data: {
     alpaca: 'The alpaca (Vicugna pacos) is a species of South American camelid mammal. It is similar to, and often confused with, the llama. However, alpacas are often noticeably smaller than llamas. The two animals are closely related and can successfully crossbreed. Both species are believed to have been domesticated from their wild relatives, the vicuña and guanaco. There are two breeds of alpaca: the Suri alpaca and the Huacaya alpaca.',
     alpacaLink: 'https://en.wikipedia.org/wiki/Alpaca',
     llama: 'The llama is a domesticated South American camelid, widely used as a meat and pack animal by Andean cultures since the Pre-Columbian era. Llamas are very social animals and live with others as a herd. Their wool is very soft and lanolin-free. Llamas can learn simple tasks after a few repetitions. When using a pack, they can carry about 25 to 30% of their body weight for 8 to 13 km (5–8 miles). The name llama (in the past also spelled lama or glama) was adopted by European settlers from native Peruvians.',
-    llamaLink: 'https://en.wikipedia.org/wiki/Llama'
-  },
-  style: {
-  color: 'white'
+    llamaLink: 'https://en.wikipedia.org/wiki/Llama',
+    style: {
+      color: 'white',
+      fontSize: "60px"
+},
   }
-})
+});
